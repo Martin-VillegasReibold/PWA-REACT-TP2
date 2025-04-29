2a10b6eb86476501f94f5e8190911f57a3ecd44a
JuanF368 <juanfarroni1314@gmail.com>
2025-04-29 14:21:01 -0300

    agregado archivo de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import Home from "./pages/Home/Home.jsx";
+import Home from "./Pages/Home/Home.jsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Details from "./pages/Details/Details.jsx";
 import Favorites from "./Pages/Favorites/Favorites.jsx";
+import { ROUTES } from "./const/routes.js";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Home />} path="/" />
-        <Route element={<Details />} path="/details" />
-        <Route element={<Favorites />} path="/favorites"/>
+        <Route element={<Home />} path={ROUTES.home} />
+        <Route element={<Details />} path={ROUTES.details} />
+        <Route element={<Favorites />} path={ROUTES.favorites}/>
       </Routes>
     </BrowserRouter>
   );
